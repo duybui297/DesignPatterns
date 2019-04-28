210c8663f2777e4aa440d65910d958af9adc3f97
khanh.duy29771@gmail.com <khanh.duy29771@gmail.com>
2019-04-28 10:34:22 +0700

    Create Order model

---
+++ DesignPatterns/DesignPatterns/Order.swift
@@ -0,0 +1,15 @@
+//
+//  Order.swift
+//  DesignPatterns
+//
+//  Created by Duy Bui on 4/28/19.
+//  Copyright © 2019 iOSTemplates. All rights reserved.
+//
+
+import Foundation
+
+struct Order {
+  var title: String
+  var quantity: Int
+  var price: Double
+}
